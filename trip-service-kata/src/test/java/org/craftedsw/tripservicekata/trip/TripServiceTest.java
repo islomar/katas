0905bd43cfc2f1605a7e2bda81b65040d4fb5f38
Isidro Lopez <islomar@gmail.com>
2015-10-06 19:49:52 +0200

    Create first test: throw UserNotLoggedInException if no logged user exists

--- trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,20 @@
 package org.craftedsw.tripservicekata.trip;
 
+
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.testng.annotations.Test;
+
+@Test
 public class TripServiceTest {
-	
+
+  @Test(expectedExceptions = UserNotLoggedInException.class)
+  public void throw_UserNotLoggedInException_if_no_user_is_logged() {
+
+    TripService tripService = new TripService();
+    User noLoggedUser = null;
+
+    tripService.getTripsByUser(noLoggedUser);
+  }
+
 }
