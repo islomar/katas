1b2d6137df1343011509a81c86c04592793774ea
Isidro Lopez <islomar@gmail.com>
2015-10-07 00:01:23 +0200

    Second test: find no trips if logged user and searched user are NOT friends

--- trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -3,25 +3,60 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.mockito.Mock;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.emptyArray;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 @Test
 public class TripServiceTest {
 
+  @Mock private User user;
+
+  @BeforeMethod
+  public void setUpMethod() {
+    initMocks(this);
+  }
+
   @Test(expectedExceptions = UserNotLoggedInException.class)
   public void throw_UserNotLoggedInException_if_no_user_is_logged() {
 
-    TripService tripService = new TesteableTripService();
     User noLoggedUser = null;
+    TripService tripService = new TesteableTripService(noLoggedUser);
 
     tripService.getTripsByUser(noLoggedUser);
   }
 
+  public void find_no_trips_if_loggedUser_and_searched_user_are_not_friends() {
+
+    TripService tripService = new TesteableTripService(user);
+    given(user.getFriends()).willReturn(new ArrayList<User>());
+
+    List<Trip> tripsByUser = tripService.getTripsByUser(user);
+    assertThat(tripsByUser, is(empty()));
+  }
+
   private class TesteableTripService extends TripService {
+
+    private final User loggedUser;
+
+    public TesteableTripService(User loggedUser) {
+
+      this.loggedUser = loggedUser;
+    }
 
     @Override
     protected User getLoggedUser() {
-      return null;
+      return this.loggedUser;
     }
   }
 
