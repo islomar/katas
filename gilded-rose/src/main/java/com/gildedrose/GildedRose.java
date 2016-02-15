c6acff7dd8714c19c11f0f9ae2a823b7869627b2
Isidro Lopez <islomar@gmail.com>
2016-02-15 21:51:22 +0100

    Remove magic numbers

--- gilded-rose/src/main/java/com/gildedrose/GildedRose.java
+++ gilded-rose/src/main/java/com/gildedrose/GildedRose.java
@@ -5,6 +5,9 @@ public class GildedRose {
     public static final String BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
     public static final String AGED_BRIE = "Aged Brie";
     public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    public static final int MAX_QUALITY = 50;
+    public static final int NUMBER_OF_DAYS_EARLIER_WITH_DOUBLE_PRIZE = 10;
+    public static final int NUMBER_OF_DAYS_EARLIER_WITH_TRIPLE_PRIZE = 5;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -21,18 +24,18 @@ public class GildedRose {
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                if (items[i].quality < MAX_QUALITY) {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn <= NUMBER_OF_DAYS_EARLIER_WITH_DOUBLE_PRIZE) {
+                            if (items[i].quality < MAX_QUALITY) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn <= NUMBER_OF_DAYS_EARLIER_WITH_TRIPLE_PRIZE) {
+                            if (items[i].quality < MAX_QUALITY) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
@@ -56,7 +59,7 @@ public class GildedRose {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (items[i].quality < MAX_QUALITY) {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
