32e2feece62a8729e26f15cd9234975bf0fcf35d
Isidro Lopez <ijlopez@alea-soluciones.com>
2016-09-28 15:30:04 +0200

    [fizzbuzz] [refactor] Remove duplicated if and use list of rules

--- fizzbuzz/python/fizzbuzz.py
+++ fizzbuzz/python/fizzbuzz.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
+from collections import OrderedDict
+
+RULES = OrderedDict([   (15, 'fizzbuzz'), 
+                        (3, 'fizz'), 
+                        (5, 'buzz')
+                    ])
 
 def process(number):
-    if _is_divisible_by(number, 15):
-        return 'fizzbuzz'    
-    if _is_divisible_by(number, 3):
-        return 'fizz'
-    if _is_divisible_by(number, 5):
-        return 'buzz'            
+    for dividend, result in RULES.iteritems():
+        if _is_divisible_by(number, dividend):
+            return result
     return str(number)
 
 def _is_divisible_by(number, divisor):
