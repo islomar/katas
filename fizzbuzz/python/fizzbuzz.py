a2f5dc4bdd0e82cc05eeb93fad9057f2b83abfd6
Isidro Lopez <ijlopez@alea-soluciones.com>
2016-09-28 15:52:57 +0200

    [fizzbuzz] [refactor] The order to apply the rules is embedded in the own rule

--- fizzbuzz/python/fizzbuzz.py
+++ fizzbuzz/python/fizzbuzz.py
@@ -3,9 +3,10 @@ from collections import OrderedDict
 
 class DivisibleByRule(object):
 
-    def __init__(self, dividend, result):
+    def __init__(self, dividend, result, priority):
         self.dividend = dividend
         self.result = result
+        self.priority = priority
 
     def applies(self, number):
         return number % self.dividend == 0
@@ -13,16 +14,19 @@ class DivisibleByRule(object):
 
 
 
-RULES =[DivisibleByRule(15, 'fizzbuzz'), 
-        DivisibleByRule(3, 'fizz'),
-        DivisibleByRule(5, 'buzz')
+RULES =[DivisibleByRule(3, 'fizz', 2),
+        DivisibleByRule(5, 'buzz', 2),
+        DivisibleByRule(15, 'fizzbuzz', 1)
 ]
 
 def process(number):
-    for rule in RULES:
+    for rule in sorted(RULES, key=_get_priority):
         if rule.applies(number):
             return rule.result
     return str(number)
 
 def _is_divisible_by(number, divisor):
     return number % divisor == 0
+
+def _get_priority(rule):
+    return rule.priority
