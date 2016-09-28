f7160e6b6df61794c68d9a73e698436ea155f903
Isidro Lopez <ijlopez@alea-soluciones.com>
2016-09-28 15:18:19 +0200

    [fizzbuzz] [refactor] Create semantic function 'is_divisible_by'

--- fizzbuzz/python/fizzbuzz.py
+++ fizzbuzz/python/fizzbuzz.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
 def process(number):
-    if number % 15 == 0:
+    if _is_divisible_by(number, 15):
         return 'fizzbuzz'    
-    if number % 3 == 0:
+    if _is_divisible_by(number, 3):
         return 'fizz'
-    if number % 5 == 0:
+    if _is_divisible_by(number, 5):
         return 'buzz'            
     return str(number)
+
+def _is_divisible_by(number, divisor):
+    return number % divisor == 0
