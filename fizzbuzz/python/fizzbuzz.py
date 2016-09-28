000729915685a95f1535df5e1572caa6d2440b47
Isidro Lopez <ijlopez@alea-soluciones.com>
2016-09-28 15:14:46 +0200

    [fizzbuzz] [pass] When processing 15 Then returns fizzbuzz

--- fizzbuzz/python/fizzbuzz.py
+++ fizzbuzz/python/fizzbuzz.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 def process(number):
+    if number == 15:
+        return 'fizzbuzz'    
     if number % 3 == 0:
         return 'fizz'
     if number % 5 == 0:
-        return 'buzz'        
+        return 'buzz'            
     return str(number)
