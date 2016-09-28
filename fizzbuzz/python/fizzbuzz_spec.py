d3cf95f33c8e1e2f8f337a1a5b18fc8662dcfef3
Isidro Lopez <ijlopez@alea-soluciones.com>
2016-09-28 10:05:26 +0200

    [fizzbuzz] [fail] When processing 2 Then returns 2

--- fizzbuzz/python/fizzbuzz_spec.py
+++ fizzbuzz/python/fizzbuzz_spec.py
@@ -7,3 +7,7 @@ with describe('Fizzbuzz'):
     with context('when processing 1'):
         with it('returns 1'):
             expect(fizzbuzz.process(1)).to(equal('1'))
+
+    with context('when processing 2'):
+        with it('returns 2'):
+            expect(fizzbuzz.process(2)).to(equal('2'))            
