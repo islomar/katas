d05888d37f3c561a6f6e0f08318357b314b2f4cc
Isidro López <islomar@gmail.com>
2018-01-29 15:21:19 +0100

    [fizzbuzz] [outside-in] [green] Should print FizzBuzz for number 15

--- fizzbuzz/java/outside-in/src/main/java/com/kata/model/FizzBuzzCalculator.java
+++ fizzbuzz/java/outside-in/src/main/java/com/kata/model/FizzBuzzCalculator.java
@@ -2,6 +2,9 @@ package com.kata.model;
 
 public class FizzBuzzCalculator {
     public String calculate(int number) {
+        if (number == 15) {
+            return "FizzBuzz";
+        }
         if (number % 3 == 0) {
             return "Fizz";
         }
