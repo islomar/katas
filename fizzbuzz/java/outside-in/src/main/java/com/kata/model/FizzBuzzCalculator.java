82bd34ac355443e3367b2fa3f2eacca55ee49f4e
Isidro López <islomar@gmail.com>
2018-01-29 13:45:31 +0100

    [fizzbuzz] [outside-in] [green] FizzBuzz should print 'Fizz' for number 6

--- fizzbuzz/java/outside-in/src/main/java/com/kata/model/FizzBuzzCalculator.java
+++ fizzbuzz/java/outside-in/src/main/java/com/kata/model/FizzBuzzCalculator.java
@@ -2,7 +2,7 @@ package com.kata.model;
 
 public class FizzBuzzCalculator {
     public String calculate(int number) {
-        if (number == 3) {
+        if (number % 3 == 0) {
             return "Fizz";
         }
         return String.valueOf(number);
