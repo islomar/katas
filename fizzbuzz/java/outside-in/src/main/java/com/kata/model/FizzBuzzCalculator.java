3f1b43c69a5cf804b4e23ab1c8a5e2485a5720ee
Isidro López <islomar@gmail.com>
2018-01-29 15:30:13 +0100

    [fizzbuzz] [outside-in] [refactor] Should print FizzBuzz for number 15

--- fizzbuzz/java/outside-in/src/main/java/com/kata/model/FizzBuzzCalculator.java
+++ fizzbuzz/java/outside-in/src/main/java/com/kata/model/FizzBuzzCalculator.java
@@ -2,15 +2,27 @@ package com.kata.model;
 
 public class FizzBuzzCalculator {
     public String calculate(int number) {
-        if (number == 15) {
+        if (isDivisibleBy3(number) && isDivisibleBy5(number)) {
             return "FizzBuzz";
         }
-        if (number % 3 == 0) {
+        if (isDivisibleBy3(number)) {
             return "Fizz";
         }
-        if (number % 5 == 0) {
+        if (isDivisibleBy5(number)) {
             return "Buzz";
         }
         return String.valueOf(number);
+    }
+
+    private boolean isDivisibleBy3(int number) {
+        return isNumberDivisibleBy(number, 3);
+    }
+
+    private boolean isDivisibleBy5(int number) {
+        return isNumberDivisibleBy(number, 5);
+    }
+
+    private boolean isNumberDivisibleBy(int number, int i) {
+        return number % i == 0;
     }
 }
