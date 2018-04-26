d2f8df60a0faea7a70547926373f19d05fd78335
Isidro López <islomar@gmail.com>
2018-04-26 21:17:06 +0200

    [roman-numerals] Convert 1 to I (GREEN)

--- roman-numerals/python/roman_numerals_spec.py
+++ roman-numerals/python/roman_numerals_spec.py
@@ -2,13 +2,16 @@
 
 from expects import *
 
+def convert_to_decimal_from_roman(decimal_number):
+    return "I"
+
 
 with describe('Roman Numerals'):
     with context('converts from decimal number to roman numeral'):
         with it('converts I to 1'):
             decimal_number = convert_to_decimal_from_roman(1)
 
-            expect(decimal_number).to("I")
+            expect(decimal_number).to(equal("I"))
 
     with context('converts fromn roman numeral to decimal number'):
         with _it('TODO'):
