a15cb948e5ec255822f3aa70a1ac5c7838515a6f
Isidro López <islomar@gmail.com>
2018-04-26 21:18:09 +0200

    [roman-numerals] Convert 2 to II (RED)

--- roman-numerals/python/roman_numerals_spec.py
+++ roman-numerals/python/roman_numerals_spec.py
@@ -3,16 +3,24 @@
 from expects import *
 
 def convert_to_decimal_from_roman(decimal_number):
-    return "I"
+    if decimal_number == 1
+        return "I"
+    return "II"
 
 
 with describe('Roman Numerals'):
-    with context('converts from decimal number to roman numeral'):
+    with context('from decimal number to roman numeral'):
         with it('converts I to 1'):
             decimal_number = convert_to_decimal_from_roman(1)
 
             expect(decimal_number).to(equal("I"))
 
-    with context('converts fromn roman numeral to decimal number'):
+        with it('converts II to 2'):
+            decimal_number = convert_to_decimal_from_roman(2)
+
+            expect(decimal_number).to(equal("II"))
+
+
+    with context('from roman numeral to decimal number'):
         with _it('TODO'):
             pass
