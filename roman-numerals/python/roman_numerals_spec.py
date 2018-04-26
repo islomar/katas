f12abf95dc948c1f0929ec506539b9dc1aea5d24
Isidro López <islomar@gmail.com>
2018-04-26 21:19:05 +0200

    Revert "[roman-numerals] Convert 2 to II (RED)"
    This reverts commit a15cb948e5ec255822f3aa70a1ac5c7838515a6f.

--- roman-numerals/python/roman_numerals_spec.py
+++ roman-numerals/python/roman_numerals_spec.py
@@ -3,24 +3,16 @@
 from expects import *
 
 def convert_to_decimal_from_roman(decimal_number):
-    if decimal_number == 1
-        return "I"
-    return "II"
+    return "I"
 
 
 with describe('Roman Numerals'):
-    with context('from decimal number to roman numeral'):
+    with context('converts from decimal number to roman numeral'):
         with it('converts I to 1'):
             decimal_number = convert_to_decimal_from_roman(1)
 
             expect(decimal_number).to(equal("I"))
 
-        with it('converts II to 2'):
-            decimal_number = convert_to_decimal_from_roman(2)
-
-            expect(decimal_number).to(equal("II"))
-
-
-    with context('from roman numeral to decimal number'):
+    with context('converts fromn roman numeral to decimal number'):
         with _it('TODO'):
             pass
