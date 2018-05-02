55bc7cde1a22a46f596fabc719fe485bacfd034c
Isidro López <islomar@gmail.com>
2018-05-02 21:36:20 +0200

    [roman-numerals] [ruby] [RED] Converts 0 to a blank string (TPP: constant->constant+)

--- roman-numerals/ruby/spec/roman_numerals_spec.rb
+++ roman-numerals/ruby/spec/roman_numerals_spec.rb
@@ -7,5 +7,9 @@ describe "Converting arabic numbers to roman numerals" do
         it "converts 0 to a blank string" do
             expect(convert(0)).to eq("")
         end
+
+        it "converts 1 to I" do
+            expect(convert(1)).to eq("I")
+        end
     end
 end
