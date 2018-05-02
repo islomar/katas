adff1a22a1a740fe872a01683e53e787c9abb0ef
Isidro López <islomar@gmail.com>
2018-05-02 21:43:32 +0200

    [roman-numerals] [ruby] [refactor] [test] Delete test duplication

--- roman-numerals/ruby/spec/roman_numerals_spec.rb
+++ roman-numerals/ruby/spec/roman_numerals_spec.rb
@@ -10,12 +10,13 @@ describe "Converting arabic numbers to roman numerals" do
             expect(convert(0)).to eq("")
         end
 
-        it "converts 1 to I" do
-            expect(convert(1)).to eq("I")
-        end
-
-        it "converts 5 to V" do
-            expect(convert(5)).to eq("V")
+        {
+            1 => "I",
+            5 => "V"
+        }.each_pair do |arabic, roman|
+            it "converts #{arabic} to #{roman}" do
+                expect(convert(arabic)).to eq(roman)
+            end
         end
     end
 end
