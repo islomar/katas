74c27e072fa59b5714c3e0968769ea1a7b7741d2
Isidro López <islomar@gmail.com>
2018-05-02 22:18:36 +0200

    [roman-numerals] [ruby] [REFACTOR] Extract conversion factors from the function

--- roman-numerals/ruby/spec/roman_numerals_spec.rb
+++ roman-numerals/ruby/spec/roman_numerals_spec.rb
@@ -1,6 +1,15 @@
+CONVERSIONS = [
+    [5, "V"],
+    [1, "I"]
+]
+
+def conversion_factors_for(in_arabic)
+    CONVERSIONS.find { |arabic, _| arabic <= in_arabic }
+end
+
 def convert(in_arabic)
     return "" if in_arabic.zero?
-    arabic, roman = [ [5, "V"], [1, "I"]].find { |arabic, _| arabic <= in_arabic }
+    arabic, roman = conversion_factors_for(in_arabic)
     roman + convert(in_arabic - arabic)
 end
 
