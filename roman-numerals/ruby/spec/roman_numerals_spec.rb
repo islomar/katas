ae071acc0f8e88bc32144cc546f8264b605f45bf
Isidro López <islomar@gmail.com>
2018-05-02 21:39:43 +0200

    [roman-numerals] [ruby] [RED] Converts 5 to V (TPP: constant->constant+)

--- roman-numerals/ruby/spec/roman_numerals_spec.rb
+++ roman-numerals/ruby/spec/roman_numerals_spec.rb
@@ -12,5 +12,9 @@ describe "Converting arabic numbers to roman numerals" do
         it "converts 1 to I" do
             expect(convert(1)).to eq("I")
         end
+
+        it "converts 5 to V" do
+            expect(convert(5)).to eq("V")
+        end
     end
 end
