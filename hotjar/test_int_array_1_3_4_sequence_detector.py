6f99baa4101e9e97c82fb9310a8610ac3373a1db
Isidro López <islomar@gmail.com>
2019-01-20 14:56:10 +0100

    Simplify logic: only for [1, 3, 4]

--- hotjar/test_int_array_1_3_4_sequence_detector.py
+++ hotjar/test_int_array_1_3_4_sequence_detector.py
@@ -2,11 +2,9 @@
 import itertools
 import unittest
 
-SEARCHED_SEQUENCE = [1, 3, 4]
-
-
 # Exercise: Given an array of ints, return True if the sequence.. 1, 3, 4 .. appears in the array somewhere
-def contains_sequence(numbers, searched_sequence):
+def contains_sequence_1_3_4(numbers):
+    searched_sequence = [1, 3, 4]
     searched_sequence_length = len(searched_sequence)
     input_length = len(numbers)
     if input_length < searched_sequence_length:
@@ -19,20 +17,21 @@ def contains_sequence(numbers, searched_sequence):
 
 # To run the tests, from the command line type:  python -test_int_array_sequence_detector.py -v
 class TestSequenceDetector(unittest.TestCase):
+    
     def test_a_long_array_containing_the_sequence_should_resturn_True(self):
-        self.assertTrue(contains_sequence([1, 3, 45, 8, 1, 3, 4, 9], SEARCHED_SEQUENCE))
+        self.assertTrue(contains_sequence_1_3_4([1, 3, 45, 8, 1, 3, 4, 9]))
 
     def test_an_array_containing_exactly_the_sequence_should_resturn_True(self):
-        self.assertTrue(contains_sequence(SEARCHED_SEQUENCE, SEARCHED_SEQUENCE))
+        self.assertTrue(contains_sequence_1_3_4([1, 3, 4]))
 
     def test_a_long_array_not_containing_the_sequence_should_resturn_False(self):
-        self.assertFalse(contains_sequence([1, 3, 45, 8, 1, 4, 9], SEARCHED_SEQUENCE))
+        self.assertFalse(contains_sequence_1_3_4([1, 3, 45, 8, 1, 4, 9]))
 
     def test_too_short_sequence_should_return_False(self):
-        self.assertFalse(contains_sequence([1, 4], SEARCHED_SEQUENCE))
+        self.assertFalse(contains_sequence_1_3_4([1, 4]))
 
     def test_an_array_containing_the_sequence_numbers_non_consecutively_should_return_False(self):
-        self.assertFalse(contains_sequence([1, 2, 3, 4], SEARCHED_SEQUENCE))
+        self.assertFalse(contains_sequence_1_3_4([1, 2, 3, 4]))
 
 if __name__ == '__main__':
     unittest.main()
