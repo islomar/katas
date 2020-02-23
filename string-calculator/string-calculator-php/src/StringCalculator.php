3cccbbb48b1eef75676e7a84a347f900943b1e62
Isidro Lopez <islomar@gmail.com>
2020-02-23 22:15:57 +0100

    Extract convertStringToInt to function

--- string-calculator/string-calculator-php/src/StringCalculator.php
+++ string-calculator/string-calculator-php/src/StringCalculator.php
@@ -1,23 +1,23 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Kata;
-
 
 
 class StringCalculator
 {
     const DELIMITER = ',';
 
-    public function add(string $numbers):int {
+    public function add(string $numbers): int
+    {
         $numbers = explode(self::DELIMITER, $numbers);
-        $convertToInt = function (string $stringElement):int {
-            return (int)$stringElement;
-        };
-        $array_map = array_map($convertToInt, $numbers);
+        $array_map = array_map(array($this, 'convertStringToInt'), $numbers);
         return array_sum($array_map);
     }
 
-
+    private function convertStringToInt(string $stringElement): int
+    {
+        return (int)$stringElement;
+    }
 }
