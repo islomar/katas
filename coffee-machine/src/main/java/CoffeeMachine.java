5f0c7f73b42fcd0603966a1e4d036525874dded4
Isidro Lopez <islomar@gmail.com>
2020-12-01 18:30:25 +0100

    [coffee-machine] [REFACTOR] extractDrinkType

--- coffee-machine/src/main/java/CoffeeMachine.java
+++ coffee-machine/src/main/java/CoffeeMachine.java
@@ -1,5 +1,6 @@
 public class CoffeeMachine {
     private final DrinkMaker drinkMaker;
+    private final String COMMAND_FORMAT = "%s:%s:%s";
 
     public CoffeeMachine(DrinkMaker drinkMaker) {
 
@@ -8,14 +9,20 @@ public class CoffeeMachine {
 
     public void orderBeverage(Beverage beverage) {
         String drinkMakerCommand = "";
-        if ("chocolate".equalsIgnoreCase(beverage.beverageType())) {
-            drinkMakerCommand += "H:";
-        }
+        String drinkType = extractDrinkType(beverage.beverageType());
+        drinkMakerCommand += drinkType;
         if (beverage.numberOfSugars() > 0) {
             drinkMakerCommand += beverage.numberOfSugars() + ":0";
         } else {
             drinkMakerCommand += ":";
         }
         this.drinkMaker.execute(drinkMakerCommand);
+    }
+
+    private String extractDrinkType(String beverageType) {
+        if ("chocolate".equalsIgnoreCase(beverageType)) {
+            return "H:";
+        }
+        return "";
     }
 }
