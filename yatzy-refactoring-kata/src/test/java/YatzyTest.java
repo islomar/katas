4b9428a9c448b8fb803ddd9477f084430801465b
Isidro Lopez <islomar@gmail.com>
2020-12-27 13:01:28 +0100

    [yatzy-refactoring-kata] Add more tests (some are temporarily commented because they fail)

--- yatzy-refactoring-kata/src/test/java/YatzyTest.java
+++ yatzy-refactoring-kata/src/test/java/YatzyTest.java
@@ -63,12 +63,14 @@ public class YatzyTest {
         assertEquals(6, Yatzy.score_pair(3, 4, 3, 5, 6));
         assertEquals(10, Yatzy.score_pair(5, 3, 3, 3, 5));
         assertEquals(12, Yatzy.score_pair(5, 3, 6, 6, 5));
+//        assertEquals(0, Yatzy.score_pair(5, 3, 6, 2, 1));
     }
 
     @Test
     public void two_Pair() {
         assertEquals(16, Yatzy.two_pair(3, 3, 5, 4, 5));
         assertEquals(16, Yatzy.two_pair(3, 3, 5, 5, 5));
+//        assertEquals(0, Yatzy.two_pair(1, 1, 2, 3, 4));
     }
 
     @Test
@@ -77,12 +79,14 @@ public class YatzyTest {
         assertEquals(15, Yatzy.three_of_a_kind(5, 3, 5, 4, 5));
         assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 5));
         assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 3));
+        assertEquals(0, Yatzy.three_of_a_kind(3, 3, 4, 5, 6));
     }
 
     @Test
     public void four_of_a_knd() {
         assertEquals(12, Yatzy.four_of_a_kind(3, 3, 3, 3, 5));
         assertEquals(20, Yatzy.four_of_a_kind(5, 5, 5, 4, 5));
+        assertEquals(0, Yatzy.four_of_a_kind(2, 2, 2, 5, 5));
     }
 
     @Test
@@ -103,5 +107,6 @@ public class YatzyTest {
     public void fullHouse() {
         assertEquals(18, Yatzy.fullHouse(6, 2, 2, 2, 6));
         assertEquals(0, Yatzy.fullHouse(2, 3, 4, 5, 6));
+        assertEquals(0, Yatzy.fullHouse(4, 4, 4, 4, 4));
     }
 }
