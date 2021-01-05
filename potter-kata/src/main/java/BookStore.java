7d02623daf87748f068e9a385ae9988690d1e73c
Isidro Lopez <islomar@gmail.com>
2021-01-05 20:09:32 +0100

    [potter-kata] [REFACTOR] Introduce map for discounts (finished... beautiful...)

--- potter-kata/src/main/java/BookStore.java
+++ potter-kata/src/main/java/BookStore.java
@@ -16,18 +16,6 @@ public class BookStore {
     public double priceFor(int... bookSeries) {
         int numberOfDifferentBookSeries = (int) IntStream.of(bookSeries).distinct().count();
         int numberOfBooksOutOfSet = bookSeries.length - numberOfDifferentBookSeries;
-        if (numberOfDifferentBookSeries == 2) {
-            return numberOfDifferentBookSeries * ONE_BOOK_BASE_PRICE_IN_EUROS * discountsForNumberOfDifferentSeries.get(numberOfDifferentBookSeries) + numberOfBooksOutOfSet * ONE_BOOK_BASE_PRICE_IN_EUROS;
-        }
-        if (numberOfDifferentBookSeries == 3) {
-            return numberOfDifferentBookSeries * ONE_BOOK_BASE_PRICE_IN_EUROS * discountsForNumberOfDifferentSeries.get(numberOfDifferentBookSeries) + numberOfBooksOutOfSet * ONE_BOOK_BASE_PRICE_IN_EUROS;
-        }
-        if (numberOfDifferentBookSeries == 4) {
-            return numberOfDifferentBookSeries * ONE_BOOK_BASE_PRICE_IN_EUROS * discountsForNumberOfDifferentSeries.get(numberOfDifferentBookSeries) + numberOfBooksOutOfSet * ONE_BOOK_BASE_PRICE_IN_EUROS;
-        }
-        if (numberOfDifferentBookSeries == 5) {
-            return numberOfDifferentBookSeries * ONE_BOOK_BASE_PRICE_IN_EUROS * discountsForNumberOfDifferentSeries.get(numberOfDifferentBookSeries) + numberOfBooksOutOfSet * ONE_BOOK_BASE_PRICE_IN_EUROS;
-        }
         return numberOfDifferentBookSeries * ONE_BOOK_BASE_PRICE_IN_EUROS * discountsForNumberOfDifferentSeries.get(numberOfDifferentBookSeries) + numberOfBooksOutOfSet * ONE_BOOK_BASE_PRICE_IN_EUROS;
     }
 }
