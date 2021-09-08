662d546b0e30f663a990b336f5333a3193572ef2
Isidro Lopez <islomar@gmail.com>
2021-09-08 13:54:42 +0200

    [parallel-changes-kata-exploration-2] Add new prices list (not used for the moment)

--- parallel-change-kata/explorations/exploration-2/src/main/java/com/pduda/field/ShoppingCart.java
+++ parallel-change-kata/explorations/exploration-2/src/main/java/com/pduda/field/ShoppingCart.java
@@ -1,7 +1,11 @@
 package com.pduda.field;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
     private int price;
+    private List<Integer> prices = new ArrayList<Integer>();
 
     public void add(int price) {
         this.price = price;
