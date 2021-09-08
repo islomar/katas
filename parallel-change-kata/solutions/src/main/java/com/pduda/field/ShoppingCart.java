5deb4cbaf167dfc53e373caa4d8d1c9b22f5f0ad
Isidro Lopez <islomar@gmail.com>
2021-09-08 12:46:25 +0200

    [parallel-change-kata] Add prices field

--- parallel-change-kata/solutions/src/main/java/com/pduda/field/ShoppingCart.java
+++ parallel-change-kata/solutions/src/main/java/com/pduda/field/ShoppingCart.java
@@ -1,6 +1,10 @@
 package com.pduda.field;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+    private static final List<Integer> prices = new ArrayList<>();
     private int price;
 
     public void add(int price) {
