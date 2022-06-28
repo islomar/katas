49383caedf1bd91a1c79372de32e9de45a92259a
Isidro Lopez <islomar@gmail.com>
2022-06-28 17:00:04 +0200

    [kata-dni] Extract constant for the required length

--- dni/src/main/java/DNI.java
+++ dni/src/main/java/DNI.java
@@ -2,13 +2,14 @@ import io.vavr.control.Either;
 
 public class DNI {
 
+    public static final int DNI_LENGTH = 9;
     private final String value;
 
     private DNI(String value) {
         this.value = value;
     }
     public static Either<DNIErrors, DNI> from(String value) {
-        if (value.length() != 9) {
+        if (value.length() != DNI_LENGTH) {
             return Either.left(new DNIErrors("The DNI should have 9 characters (no more, no less)"));
         }
         return Either.right(new DNI(value));
