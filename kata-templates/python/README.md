a01b4dc573e3dd9dd62646706ab97781e367d1fb
Isidro Lopez <islomar@gmail.com>
2024-02-13 21:21:50 +0100

    Move Python template to an independent GitHub repository

---
+++ kata-templates/python/README.md
@@ -0,0 +1,1 @@
+You can find a Python template [here](https://github.com/islomar/python-kata-template)
--- kata-templates/python/src/foo.py
+++
@@ -1,3 +0,0 @@
-class Foo:
-    def add(self, first_addend: int, second_addend: int) -> int:
-        return first_addend + second_addend
